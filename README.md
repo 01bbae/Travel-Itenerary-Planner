782e24a44547f3d850316b4ebcb6304e0fb4209c
BJ <01bbae@gmail.com>
2022-11-05 01:34:16 -0700

    Initializing Project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Travel-Itenerary-Planner
+
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  // function initMap() {
+  //   var directionsService = new google.maps.DirectionsService();
+  //   var directionsRenderer = new google.maps.DirectionsRenderer();
+  //   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  //   var mapOptions = {
+  //     zoom:7,
+  //     center: chicago
+  //   }
+  //   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  //   directionsRenderer.setMap(map);
+  // }
+  
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
