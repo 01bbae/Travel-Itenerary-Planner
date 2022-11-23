a8a2019e17630eb91292a60f04059ced9dd70e02
TylerKay <tkay@chapman.edu>
2022-11-22 16:13:21 -0800

    Added origin and destination inputs. Working on autocomplete google map functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Input from "./Inputs";
 import Map from "./Map";
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Input />
       <Map />
     </div>
--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,12 +1,34 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   GoogleMapProvider,
   userGoogleMap,
 } from "@ubilabs/google-maps-react-hooks";
-// import { GoogleMap, userLoadScript, Marker, useLoadScript } from '@react-google-maps/api'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react'
+import { GoogleMap, userLoadScript, Marker, useLoadScript, Autocomplete, DirectionsRenderer, useJsApiLoader } from '@react-google-maps/api'
+
+// Initialize center
+const center = { lat: 33.79, lng: -117.85, }
 
 const Map = () => {
+  // const [ libraries ] = useState(['places']);
+  const originRef = useRef()
+  const destiantionRef = useRef()
+
+  const [map, setMap] = useState((null))
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
   const [mapContainer, setMapContainer] = useState(null);
 
   const mapOptions = {
@@ -40,14 +62,101 @@ const Map = () => {
   //   return <GoogleMap zoom={10} center={{lat:44, lng: -80}} mapContainerClassName = "map-container"></GoogleMap>
   // }
 
+  async function calculateRoute() {
+    if (originRef.current.value === '' || destiantionRef.current.value === '') {
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destiantionRef.current.value = ''
+  }
+
   return (
-    <GoogleMapProvider
-      googleMapsAPIKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-      options={mapOptions}
-      mapContainer={mapContainer}
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      h = '100vh'
+      w = '100vw'
     >
-      <div ref={(node) => setMapContainer(node)} style={{ height: "100vh" }} />
-    </GoogleMapProvider>
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+          
+
+        <GoogleMapProvider
+          googleMapsAPIKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+          options={mapOptions}
+          mapContainer={mapContainer}
+        >
+        <div ref={(node) => setMapContainer(node)} style={{ height: "100vh" }} />
+        </GoogleMapProvider>
+      </Box>
+
+      <Box
+        p={4}
+        borderRadius='lg'
+        m={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='1'
+      >
+        <HStack spacing={2} justifyContent='space-between'>
+          <Box flexGrow={1}>
+            {/* <Autocomplete> */}
+              <Input type='text' placeholder='Origin' ref={originRef} />
+            {/* </Autocomplete> */}
+          </Box>
+          <Box flexGrow={1}>
+            {/* <Autocomplete> */}
+              <Input
+                type='text'
+                placeholder='Destination'
+                ref={destiantionRef}
+              />
+            {/* </Autocomplete> */}
+          </Box>
+
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              // icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: {distance} </Text>
+          <Text>Duration: {duration} </Text>
+          <IconButton
+            aria-label='center back'
+            // icon={<FaLocationArrow />}
+            isRound
+            onClick={() => {
+              map.panTo(center)
+              map.setZoom(15)
+            }}
+          />
+        </HStack>
+      </Box>
+    </Flex>
   );
 };
 
