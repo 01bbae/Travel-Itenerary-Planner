48b743012cf0f22b014779f6a2c446312833b341
TylerKay <tkay@chapman.edu>
2022-11-23 22:57:53 -0800

    Different routes can now be changed hardcoded by randomized waypoints

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -15,7 +15,7 @@ import {
   SkeletonText,
   Text,
 } from '@chakra-ui/react'
-import { GoogleMap, userLoadScript, Marker, useLoadScript, Autocomplete, DirectionsRenderer, useJsApiLoader } from '@react-google-maps/api'
+import { GoogleMap, userLoadScript, Marker, useLoadScript, Autocomplete, DirectionsRenderer, useJsApiLoader, Polyline } from '@react-google-maps/api'
 
 // Initialize center
 const center = { lat: 33.79, lng: -117.85, }
@@ -68,15 +68,61 @@ const Map = () => {
     }
     // eslint-disable-next-line no-undef
     const directionsService = new google.maps.DirectionsService()
-    const results = await directionsService.route({
+    var results = await directionsService.route({
       origin: originRef.current.value,
       destination: destiantionRef.current.value,
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
+      // waypoints: [{ stopover: true, location: { placeId: "ChIJRVj1dgPP20YRBWB4A_sUx_Q" } }],
+      provideRouteAlternatives: true,
     })
-    setDirectionsResponse(results)
+
+    // setDirectionsResponse(results)
     setDistance(results.routes[0].legs[0].distance.text)
     setDuration(results.routes[0].legs[0].duration.text)
+    console.log("Route 0: ");
+    console.log(results.routes[0])
+    console.log(results.routes[0].overview_polyline)
+    console.log("Route 1: ")
+    console.log(results.routes[1])
+    console.log(results.routes[1].overview_polyline)
+    
+    var decodedPoly = decode(results.routes[1].overview_polyline);
+    
+    // Get random index for waypoints
+    var randomIndexes = []
+    var num;
+    for (let i = 0; i < 25; ++i){
+      num = Math.floor(Math.random() * decodedPoly.length);
+      randomIndexes.push(num)
+    }
+    for (let i = 0; i < randomIndexes.length; ++i){
+      console.log(randomIndexes[i])
+    }
+
+
+    // Get wayPoints (lat, lng) in an array
+    var wayPoints = []
+    for (let i = 0; i < randomIndexes.length; ++i){ // Max waypoints you can add is 25
+      let curr_lat = decodedPoly[randomIndexes[i]]["latitude"]
+      let curr_lng = decodedPoly[randomIndexes[i]]["longitude"]
+      // eslint-disable-next-line no-undef
+      var wayPoint = new google.maps.LatLng({lat:curr_lat, lng: curr_lng});
+      wayPoints.push({location: wayPoint, stopover: false})
+    }
+
+    // Send another request, this time for the route we want to display with an added waypoint parameter
+    const results2 = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+      // waypoints: [{ stopover: true, location: { placeId: "ChIJRVj1dgPP20YRBWB4A_sUx_Q" } }],
+      provideRouteAlternatives: true,
+      waypoints: wayPoints
+    })
+    setDirectionsResponse(results2)
+    console.log(results2)
   }
   function clearRoute() {
     setDirectionsResponse(null)
@@ -84,6 +130,40 @@ const Map = () => {
     setDuration('')
     originRef.current.value = ''
     destiantionRef.current.value = ''
+  }
+
+  // Function which decodes the overview_polyline and returns coordinates.
+  function decode(encoded){ // Reference: https://gist.github.com/ismaels/6636986
+    // array that holds the points
+    var points=[ ]
+    var index = 0, len = encoded.length;
+    var lat = 0, lng = 0;
+    while (index < len) {
+      var b, shift = 0, result = 0;
+      do {
+        b = encoded.charAt(index++).charCodeAt(0) - 63;//finds ascii                                                                                    //and substract it by 63
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+
+
+      var dlat = ((result & 1) !== 0 ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.charAt(index++).charCodeAt(0) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      
+      var dlng = ((result & 1) !== 0 ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+  
+      points.push({latitude:( lat / 1E5),longitude:( lng / 1E5)})  
+      
+    }
+    return points
   }
 
   const { isLoaded } = useJsApiLoader({
